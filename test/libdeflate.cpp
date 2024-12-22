6200da2b0cb6d6b1627e0a9e59568bb597012608
HyperBitGore <bruiserjoe01@gmail.com>
2024-12-22 02:43:53 -0600

    Fixed build on windows

--- test/libdeflate.cpp
+++ test/libdeflate.cpp
@@ -1,6 +1,6 @@
 #include "../include/deflate.hpp"
 #include "../include/inflate.hpp"
-#include <libdeflate.h>
+#include "../build/external/include/libdeflate.h"
 
 
 void writeBufferToFile (char* data, size_t size, std::string file_path) {
@@ -12,7 +12,11 @@ void writeBufferToFile (char* data, size_t size, std::string file_path) {
     file.close();
 }
 
-
+char data[32490];
+char out_data_lib[32490];
+char out_data_inhpp[32490];
+char out_data_hpp[32490];
+char out_inflate_lib[32490];
 
 int main () {
 
@@ -20,26 +24,23 @@ int main () {
 
     std::ifstream file;
     file.open("test.bmp", std::ios::binary);
-
     if (!file) {
+        std::cout << "Error can't open test.bmp file!" << std::endl;
         return -1;
     }
-    char data[32490];
     file.read(data, 32490);
 
     size_t sizef = file.gcount();
     libdeflate_compressor* compressor = libdeflate_alloc_compressor(1);
-    char out_data_lib[32490];
     //testing the two
     size_t size_lib = libdeflate_deflate_compress(compressor, data, sizef, out_data_lib, 32490);
     std::cout << "size of libdeflate: " << size_lib << "\n";
 
-    char out_data_inhpp[32490];
 
     size_t sizein_hpp = inflate::decompress(out_data_lib, size_lib, out_data_inhpp, 32490);
     std::cout << "size of inflate.hpp: " << sizein_hpp << "\n";
     writeBufferToFile(out_data_inhpp, sizein_hpp, "hpptestinflate.bmp");
-    char out_data_hpp[32490];
+
     size_t size_hpp = deflate::compress(data, sizef, out_data_hpp, 32490);
     std::cout << "size of deflate.hpp: " << size_hpp << "\n";
     //decompressing the data
